dc92bd800d51e8e3a9d732f8af37d04c7395a411
Ygor Magalhães <emaildoygor@gmail.com>
2019-06-20 09:07:05 -0300

    WIP

--- src/main/java/br/com/opah/broker/controllers/TravelController.java
+++ src/main/java/br/com/opah/broker/controllers/TravelController.java
@@ -1,22 +1,28 @@
 package br.com.opah.broker.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.opah.broker.dto.HotelDTO;
 import br.com.opah.broker.services.TravelService;
 
 @RestController
-public class TravelController {
+public class TravelController { // TODO: Ajustar path ex.: /api/v1/...
 
     @Autowired()
     private TravelService travelService;
 
     @GetMapping("/travel")
-    public List<HotelDTO> travel() {
+    public List<HotelDTO> getTravelInfo(
+        @RequestParam("CityCode") int cityCode,
+        @RequestParam("Checkin") @DateTimeFormat(pattern = "YYYY-MM-DD") Date checkin) {
+        System.out.println(checkin);
         return this.travelService.getHotels();
     }
 }
